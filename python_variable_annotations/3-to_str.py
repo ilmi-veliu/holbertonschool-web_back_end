978f5c69aec7daef88cb3704ec0b5be09b53355f
ilmi-veliu <iveliu011@gmail.com>
2025-07-28 16:12:21 +0200

    3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def to_str(a: float) -> str:
+    """Convert a float to a string.
+
+    Args:
+        a (float): The float to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(a)
