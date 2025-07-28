b87e977fb688df4bc3cfcbc2d43331af1e69c1e2
ilmi-veliu <iveliu011@gmail.com>
2025-07-28 15:55:59 +0200

    1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module concat
+Provides a function to concatenate two strings.
+"""
+
+def concat(a: str, b: str) -> str:
+    """Concatenate two strings and return the result.
+
+    Args:
+        a (str): The first string.
+        b (str): The second string.
+
+    Returns:
+        str: The concatenation of a and b.
+    """
+    return a + b
