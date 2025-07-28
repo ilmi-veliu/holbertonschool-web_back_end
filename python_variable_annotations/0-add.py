e0bc18ccdc00476405d87facae68cef59d95fe59
ilmi-veliu <iveliu011@gmail.com>
2025-07-28 15:44:29 +0200

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# add - Additionne deux nombres à virgule
+# @a: premier nombre (float)
+# @b: second nombre (float)
+# Return: la somme des deux nombres (float)
+def add(a: float, b: float) -> float:
+    return a + b
