46bad23e509f10510cf875195a4b6ca6f2e347e5
ilmi-veliu <iveliu011@gmail.com>
2025-07-28 16:00:38 +0200

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module floor
+Provides a function that returns the floor of a float number.
+"""
+
+def floor(n: float) -> int:
+    """Return the floor of a float number as an integer.
+
+    Args:
+        n (float): The float number to convert.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return int(n)
