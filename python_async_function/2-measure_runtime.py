c0c8134c1a4d767da09020afe4a8eaa3e9402706
ilmi-veliu <iveliu011@gmail.com>
2025-07-30 21:10:53 +0200

    2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,11 @@
+import time
+import asyncio
+from typing import List
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n : int , max_delay :int ) ->float:
+    start = time.time()
+    await wait_n(n,max_delay)
+    end = time.time()
+    total_time = end - start
+    return (total_time / n)
