839be1bb6edd95370759980626838bb79993f708
ilmi-veliu <iveliu011@gmail.com>
2025-07-31 15:23:27 +0200

    inversion entre sleep et yield

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -16,5 +16,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
         float: Random number between 0 and 10
     """
     for i in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
