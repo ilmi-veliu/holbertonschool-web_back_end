4210208f6cd2e3395a5f16416d15c7cffaeea4ad
ilmi-veliu <iveliu011@gmail.com>
2025-08-19 11:42:27 +0200

    s

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    var task = false;
+    var task2 = true;
   
     if (trueOrFalse) {
       let task = true;
       let task2 = false;
     }
- 
+
     return [task, task2];
- }
- 
+}
