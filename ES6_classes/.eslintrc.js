2f912aa3e9eeab371121ced313715410a9a42419
ilmi-veliu <iveliu011@gmail.com>
2025-08-22 15:04:14 +0200

    0

---
+++ ES6_classes/.eslintrc.js
@@ -0,0 +1,47 @@
+// .eslintrc.js
+module.exports = {
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    settings: {
+      // Permet à eslint-plugin-import de résoudre les imports avec extension .js
+      'import/resolver': {
+        node: { extensions: ['.js'] },
+      },
+    },
+    rules: {
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-new': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+      // Autorise l’extension .js pour les imports locaux
+      'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
+    },
+    overrides: [
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      },
+    ],
+  };
