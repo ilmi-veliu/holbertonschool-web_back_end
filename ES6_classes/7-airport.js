fe966e730bfe0fb25e02abbea138d69b6fe587cd
ilmi-veliu <iveliu011@gmail.com>
2025-08-24 20:02:06 +0200

    7 / 8

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+    constructor(name, code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = newName;
+    }
+    get name() {
+        return this._name;
+    }
+
+    set code(newCode) {
+        if (typeof newCode !== 'string') {
+            throw new TypeError('Code must be a string');
+        }
+        this._code = newCode;
+    }
+    get code() {
+        return this._code;
+    }
+
+    toString() {
+      return `[object ${this._code}]`;
+    }
+}
