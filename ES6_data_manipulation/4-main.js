cde512799754b98adedbde2ac3869a7065c7d75a
ilmi-veliu <iveliu011@gmail.com>
2025-08-27 00:20:18 +0200

    4

--- ES6_data_manipulation/4-main.js
+++ ES6_data_manipulation/4-main.js
@@ -1,6 +1,6 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-    if (!Array.isArray(students) || !Array.isArray(newGrades)) {
-        return [];
-    }
-    
-}
+import getListStudents from './0-get_list_students.js';
+import updateStudentGradeByCity from './4-update_grade_by_city.js';
+
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }]));
