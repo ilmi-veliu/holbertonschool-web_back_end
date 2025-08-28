c80a668e24be978e3e656f0ac8d349b40cbbab58
ilmi-veliu <iveliu011@gmail.com>
2025-08-28 11:30:44 +0200

    0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const response = true;
-    if (response) {
-      resolve('data received');
-    } else {
-      reject('API error');
-    }
-  });
+  return new Promise(() => {});
 }
