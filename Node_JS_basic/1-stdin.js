b97a3d4fd07300d89c23d3a5ec0c53d025402d38
ilmi-veliu <iveliu011@gmail.com>
2025-09-02 09:14:36 +0200

    1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.setEncoding("utf-8");
+
+process.stdin.on("data", (data) => {
+    const name = data.trim();
+    console.log(`Your name is: ${name}\n`);
+    process.exit();
+});
+
+process.on("exit", () => {
+    console.log("This important software is now closing\n");
+});
